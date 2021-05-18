8d6e57dea66568e0b31a36c3d3f5553992593512
Tora Sirkka <tora@torasirkka.fi>
2021-05-17 18:58:02 -0700

    Completed javascript functions in functions.js.

--- functions.js
+++ functions.js
@@ -2,19 +2,44 @@
 
 
 // 1. isHometown
-
 function isHometown(town) {
   return town === "San Francisco";  
 }
 
 // 2. getFullName
-
 function getFullName(first, last) {
     return `${first} ${last}`;
 }
 
-
-
 // 3. calculateTotal
+const calculateTotal = (base_price, state, tax = 0.05) => {
+  /** Return the total price of an item, figuring in state taxes and fees.
 
-// Define your function here
+  Arguments:
+      base_price (float): Base price of an item
+      state (str): Two-letter abbreviation of a U.S. state
+      tax (float): Tax rate. Defaults to 0.05
+
+  Returns:
+      float: The total price of an item
+  */
+
+  let subtotal = base_price * (1 + tax);
+
+  let fee = 0;
+  if (state === 'CA') {
+    fee = 0.03 * subtotal;
+  } else if (state == 'MA') {
+      if (base_price <= 100) {
+        fee = 1;
+      }
+      else {
+        fee = 3;
+      }
+  } else if (state === 'PA') {
+    fee = 2;
+  }
+
+  return subtotal + fee ;
+};
+
